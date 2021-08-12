dcf4dca85c058bbebb65e98072e8b00ada65cf2b
vnextDevops <88445524+vnextDevops@users.noreply.github.com>
2021-08-12 22:17:19 +0530

    Add files via upload

---
+++ firewall Status Update.ps1
@@ -0,0 +1,34 @@
+﻿$workspaceName = "wvdeus2loganalytics"
+$workspaceRG = "wvdeus2"
+
+$WorkspaceID = (Get-AzOperationalInsightsWorkspace -ResourceGroupName $workspaceRG -Name $workspaceName).CustomerId
+$query = 'AzureMetrics
+| where MetricName contains "FirewallHealth" 
+|where Resource contains "WVDCENTRALINDIAFIREWALL" or Resource contains "WVDWESTEUROPEFIREWALL" or Resource contains "WVDEASTASIAFIREWALL"
+| top 100 by TimeGenerated
+| project TimeGenerated, Resource , MetricName , Average'
+$results = Invoke-AzOperationalInsightsQuery -WorkspaceId $WorkspaceID -Query $query
+
+$results = [System.Linq.Enumerable]::ToArray($results.Results)
+
+$firewall = Get-AzFirewall
+$firewall = $firewall.name
+$final_Result = @()
+foreach($item in $firewall)
+{
+$final_Result += $results | Where-Object {$_.Resource -eq "$item"} | Select-Object -Unique
+}
+
+Foreach($item in $final_Result)
+{
+$resource_name = $item.Resource
+[Int]$Average = $item.Average
+if($Average -lt '95')
+{Write-Host $resource_name "is Degraded"}
+elseif($Average -lt 99)
+{Write-Host "$resource_name is Unhealthy"}
+else
+{Write-Host "$resource_name is healthy"}
+}
+
+
